344797fcb92b303279d6425831cf6d29f12f2479
ErwannGui <erwann.guillevic@ynov.com>
2019-10-17 14:40:26 +0200

    init commit & menu + navigation

---
+++ PointerDetect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class PointerDetect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		Debug.Log("OnMouseEnter");
+		GetComponent<Text>().color = Color.red;
+	}
+	
+	public void OnPointerExit(PointerEventData eventData) {
+		Debug.Log("OnMouseExit");
+		GetComponent<Text>().color = Color.white;
+    }
+}
---
+++ SceneSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void SelectLevel()
+    {
+        SceneManager.LoadScene("Level");
+    }
+	
+	public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+	
+	public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+	
+    public void ReturntoMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+	
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
